d13135737eaa38550d459b2ebdab6a2b334fd36b
zahidjos <zahidhasanjos90@gmail.com>
2022-05-24 13:13:36 +0600

    delete is complete

--- index.js
+++ index.js
@@ -44,6 +44,12 @@ async function run() {
 
 
       })
+      app.delete('/order/:id',async(req,res)=>{
+        const id=req.params.id;
+        const query = {_id:ObjectId(id)};
+        const service=await orderCollection.deleteOne(query);
+        res.send(service);
+   })
 
       
       
