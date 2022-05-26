c7ab3ff85531f00de60418551f29407c8e77023a
zahidjos <zahidhasanjos90@gmail.com>
2022-05-26 19:13:26 +0600

    nodemon aded

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
  const jwt = require('jsonwebtoken');
  require('dotenv').config();
  const stripe = require("stripe")(process.env.STRIPE_KEY)
- const port=process.env.PORT||5000
+ const port=process.env.PORT||5000;
  app.use(cors());
  app.use(express.json());
 
