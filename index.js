3bbc48af4bc4a8a4052ca52b7ca0299094b98e2c
zahidjos <zahidhasanjos90@gmail.com>
2022-05-26 20:03:16 +0600

    heroky payment secret problem

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
  const app=express();
  const jwt = require('jsonwebtoken');
  require('dotenv').config();
- const stripe = require("stripe")(process.env.STRIPE_KEY)
+ const stripe = require("stripe")('sk_test_51L3UeFGM6w2wMAOdTLAhx4EJBbNk2hXxdboxgOiG8xpWInXZKAdOwBlMl5eouNRshcTsXLdGPkQAFiOSOdGCy7Ks00hfMmQJqt')
  const port=process.env.PORT||5000;
  app.use(cors());
  app.use(express.json());
