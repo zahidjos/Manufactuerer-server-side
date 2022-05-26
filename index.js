6b8069ac18dee8756cda523a9cf78eefe30c7ba6
zahidjos <zahidhasanjos90@gmail.com>
2022-05-26 20:16:32 +0600

    deploy problem

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
  const express=require('express')
-//  const cors=require('cors');
+ const cors=require('cors');
  const app=express();
  const jwt = require('jsonwebtoken');
  require('dotenv').config();
  const stripe = require("stripe")('sk_test_51L3UeFGM6w2wMAOdTLAhx4EJBbNk2hXxdboxgOiG8xpWInXZKAdOwBlMl5eouNRshcTsXLdGPkQAFiOSOdGCy7Ks00hfMmQJqt')
  const port=process.env.PORT||5000;
-//  app.use(cors());
+ app.use(cors());
  app.use(express.json());
 
  
